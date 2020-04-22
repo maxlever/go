6f781da296874d0c9865c2e805db81fccf0cdd41
Max Lever <max.lever@axios.com>
2020-04-22 10:30:44 -0400

    add attempt

--- exercises/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercises/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -11,4 +13,32 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+// SyncFrequency counts the frequency of each rune in a given text
+// and mutates the given sync.Map
+// sideeffect: decrements given wait group
+func SyncFrequency(str string, waitGroup *sync.WaitGroup, m *sync.Map) {
+	for _, letter := range str {
+		oldValue, _ := m.LoadOrStore(letter, 0)
+		m.Store(letter, oldValue.(int)+1)
+	}
+	waitGroup.Done()
+}
+
+// ConcurrentFrequency gets the total frequency of runes in the given strings
+func ConcurrentFrequency(strings []string) FreqMap {
+	endMap := make(FreqMap)
+	concurrentMap := sync.Map{}
+	waitGroup := sync.WaitGroup{}
+	for _, str := range strings {
+		waitGroup.Add(1)
+		go SyncFrequency(str, &waitGroup, &concurrentMap)
+	}
+	waitGroup.Wait()
+	concurrentMap.Range(func(k interface{}, v interface{}) bool {
+		endMap[k.(rune)] = v.(int)
+		return true
+	})
+	return endMap
 }
